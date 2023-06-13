212ca4ac123ea33fec282ee49d3cf0d000c44898
Đoàn Đức Thắng <ducthang8558@gmail.com>
2023-06-13 16:02:43 +0700

    main

--- test.cpp
+++ test.cpp
@@ -1,40 +1,66 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
+#include <vector>
+#include <algorithm>
 
-int binaryToDecimal(string binary) {
-    int decimal = 0;
-    int power = 0;
-    
-    for (int i = binary.length() - 1; i >= 0; i--) {
-        if (binary[i] == '1') {
-            decimal += pow(2, power);
-        }
-        power++;
+std::vector<int> getDigits(int num) {
+    std::vector<int> digits;
+    while (num > 0) {
+        digits.push_back(num % 10);
+        num /= 10;
     }
-    
-    return decimal;
+    std::reverse(digits.begin(), digits.end());
+    return digits;
 }
 
-int multiplyBinaryStrings(string s1, string s2) {
-    int num1 = binaryToDecimal(s1);
-    int num2 = binaryToDecimal(s2);
-    
-    int product = num1 * num2;
-    
-    return product;
+int countDigits(int num) {
+    int count = 0;
+    while (num > 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+int factorial(int num) {
+    if (num == 0 || num == 1) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+bool hasNDigitsFactorial(int N, int num) {
+    int fact = factorial(num);
+    int digitsCount = countDigits(fact);
+    return digitsCount == N;
 }
 
 int main() {
-    int T;
-    cin >> T;
-    cin.ignore();
-    while (T--) {
-        string s1, s2;
-        cin >> s1 >> s2;
-        int result = multiplyBinaryStrings(s1, s2);
-        cout << result << endl;
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+        int N;
+        std::cin >> N;
+
+        std::vector<int> X_list;
+
+        int start = 1;
+        int end = 1;
+        for (int i = start; i <= end; i++) {
+            if (hasNDigitsFactorial(N, i)) {
+                X_list.push_back(i);
+            }
+        }
+
+        if (X_list.empty()) {
+            std::cout << "NO" << std::endl;
+        } else {
+            std::cout << X_list.size() << std::endl;
+            for (int X : X_list) {
+                std::cout << X << std::endl;
+            }
+        }
     }
-    
+
     return 0;
 }
