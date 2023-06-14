3a4d704c921bc351e6180fca74f04b0e92bc6896
Đoàn Đức Thắng <ducthang8558@gmail.com>
2023-06-14 10:23:32 +0700

    change

--- test.cpp
+++ test.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        
+    int n;
+    //cin >> n;
+    //vector<int> a[n];
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    //cout << matrix.size();
+    cout << endl;
+    for (int i=0; i<= matrix.size()-1; i++){
+        for(int j = 0; j <= matrix.size()-1; j++){
+            //cout << matrix[i][j]<< ' ';
+        }
+        //cout << endl;
     }
+    cout << matrix[0][1];
 }
